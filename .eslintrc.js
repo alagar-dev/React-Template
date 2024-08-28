bd3287d7956d81e921571ab298b6f95eb75baf72
Kyle Corbin <alagar@Kyles-Laptop.attlocal.net>
2024-08-28 16:22:08 -0400

    set up prettier and eslint. working individually but no local ci/cd set up yet

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+    '@react-native-community',
+    'react',
+    'react-native',
+    'jest',
+    'prettier',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    '@react-native-community',
+    'plugin:react/recommended',
+    'plugin:react-native/all',
+    'plugin:jest/recommended',
+    'plugin:prettier/recommended',
+  ],
+  env: {
+    es2021: true,
+    node: true,
+    'react-native/react-native': true,
+  },
+  rules: {
+    'react/prop-types': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'react-native/no-unused-styles': 'warn',
+    'react-native/split-platform-components': 'warn',
+    'react-native/no-inline-styles': 'warn',
+    'react-native/no-color-literals': 'warn',
+    'react-native/no-raw-text': ['warn', { skip: ['CustomText'] }],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
